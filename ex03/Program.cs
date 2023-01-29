72f460537a3ef925f5244aa91c11d0ec2d761d1e
AndreySh <shingaryovan@gmail.com>
2023-01-30 00:43:54 +1200

    Показать числа Фибоначчи меньшие заданного числа N

---
+++ ex03/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать числа Фибоначчи меньшие заданного числа N
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int[] fib = new int[N];
+
+for (int i = 0; i < N; i++)
+{
+    if (i == 0 || i == 1) fib[i] = 1;
+    else fib[i] = fib[i-1] + fib[i - 2];
+    if (fib[i] >= N) break;
+    else Console.WriteLine(fib[i]);
+}
