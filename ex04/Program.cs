83072acab7faaa1decd03fb79f8a381725b9a4c1
AndreySh <shingaryovan@gmail.com>
2023-01-30 00:53:10 +1200

    оказать двумерный массив размером m × n заполненный целыми числами

---
+++ ex04/Program.cs
@@ -0,0 +1,34 @@
+﻿// Показать двумерный массив размером m × n заполненный целыми числами
+void FillMatrix(int[,] arg)
+{
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {
+        for (int j = 0; j < arg.GetLength(1); j++)
+        {
+            arg[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] arg)
+{
+    for (int i = 0; i < arg.GetLength(0); i++)
+    {
+        for (int j = 0; j < arg.GetLength(1); j++)
+        {
+            Console.Write($" {arg[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите число m: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[m,n];
+
+FillMatrix(matrix);
+
+PrintMatrix(matrix);
