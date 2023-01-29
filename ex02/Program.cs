914546d4dc5e768f1c395e2476cffd3bb714f319
AndreySh <shingaryovan@gmail.com>
2023-01-30 00:01:49 +1200

    преобразование десятичного числа в двоичное

---
+++ ex02/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу преобразования десятичного числа в двоичное
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int copy = n;
+
+string bin = "";
+while (copy >= 1)
+{
+    bin = copy % 2 + bin; 
+    copy = copy / 2;
+}
+
+Console.WriteLine(bin);
