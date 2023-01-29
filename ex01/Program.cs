5619176002f5f23172bb992fc39bb3fba98e1316
AndreySh <shingaryovan@gmail.com>
2023-01-29 23:27:36 +1200

    задание проверки является ли три числа сторонами треугольника

---
+++ ex01/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выяснить являются ли три числа сторонами треугольника
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+if ( ((a + b) > c) && (a + c > b) && (b + c > a))
+{
+    Console.WriteLine("три числа являются сторонами треугольника");
+}
+else
+{
+    Console.WriteLine("три числа НЕ являются сторонами треугольника");
+} 
+
